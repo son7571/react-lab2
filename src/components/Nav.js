eb4ec418ea1ac5e48f7537d571eb07cb15cdc1c0
son7571 <sssk1755@gmail.com>
2025-08-20 11:35:27 +0900

    save

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Nav() {
+    return (
+        <NavStyle>
+            <NavLink to="/">홈</NavLink>
+            <NavLink to="/about">소개</NavLink>
+            <NavLink to="/login">로그인</NavLink>
+        </NavStyle>
+    );
+}
+
+// Html 태그가 아닌것들은 괄호에!!
+const NavLink = styled(Link)`
+    margin-right: 10px;
+    text-decoration: none;
+`;
+
+const NavStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+`;
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Home() {
+    console.log("랜더링");
+    // 배열 구조 할당
+    const [num, setNum] = useState(0);
+
+    function add() {
+        let newNum = num + 1;
+        setNum(newNum);
+    }
+    function minus() {
+        let newNum = num - 1;
+        setNum(newNum);
+    }
+
+    return (
+        <div>
+            <div>상태관리연습</div>
+            <div>{num}</div>
+            <button onClick={add}>+1</button>
+            <button onClick={minus}>-1</button>
+            <hr />
+        </div>
+    );
+}
